61f3303aba7e5dca02eaeda663641495a1258661
HafsaSultana <50019589+HafsaSultana@users.noreply.github.com>
2021-01-18 01:40:14 +0600

    Add files via upload

---
+++ Data-Structure-Algorithom/Week-1/Linked List.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+int main()
+{
+    int a[5];
+    for(int i=0;i<5;i++)
+    {
+        printf("%u\n",&(a[i])); //print address of each element
+    }
+    return 0;
+}
+*/
+
+
+struct node
+{
+  int roll;
+	node *next;
+};
+node *root=NULL;
+int main(){
+
+    return 0;
+}
+
+
+
+
+
+
