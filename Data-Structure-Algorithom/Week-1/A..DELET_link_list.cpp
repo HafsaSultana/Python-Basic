cd80901ee6cb50e123042f91c7096d06d945ef69
HafsaSultana <hafsa1720@cseku.ac.bd>
2021-01-22 02:10:59 +0600

    Create A..DELET_link_list.cpp

---
+++ Data-Structure-Algorithom/Week-1/A..DELET_link_list.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<malloc.h>
+struct node
+{
+    int data;
+    struct node *link;
+};
+int main()
+{
+     struct node *t,*f,*e;
+     struct node *t1,*t2,*t3;
+
+     for(int i=0;i<5;i++)
+     {
+         t=(struct node*)malloc(sizeof(node));
+         //t->data=i+1;
+         scanf("%d",&t->data);
+         t->link=NULL;
+         if(i==0)
+         {
+             f=t;
+         }
+         else
+         {
+            e->link=t;
+         }
+         e=t;
+     }
+     t1=f->link;
+     t2=f->link->link->link;
+     t1->link=t2;
+     while(f!=NULL)
+     {
+         printf("%d  ",f->data);
+         f=f->link;
+     }
+}
