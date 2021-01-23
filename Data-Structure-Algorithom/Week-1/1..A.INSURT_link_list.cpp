f7d1ea6608dab1854b0bb652927b680686d00e8e
HafsaSultana <hafsa1720@cseku.ac.bd>
2021-01-24 01:09:23 +0600

    Update 1..A.INSURT_link_list.cpp

--- Data-Structure-Algorithom/Week-1/1..A.INSURT_link_list.cpp
+++ Data-Structure-Algorithom/Week-1/1..A.INSURT_link_list.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 #include<malloc.h>
 struct node
@@ -39,3 +40,46 @@ int main()
          f=f->link;
      }
 }
+=======
+#include<stdio.h>
+#include<malloc.h>
+struct node
+{
+    int data;
+    struct node *link;
+};
+int main()
+{
+     struct node *t,*f,*e;
+     struct node *t1,*t2,*t3;
+
+     for(int i=0;i<5;i++)
+     {
+         t=(struct node*)malloc(sizeof(node));
+         //t->data=i+1;
+         scanf("%d",&t->data);
+         t->link=NULL;
+         if(i==0)
+         {
+             f=t;
+         }
+         else
+         {
+            e->link=t;
+         }
+         e=t;
+     }
+      t3=(struct node*)malloc(sizeof(node));
+      scanf("%d",&t3->data);
+      //t3->data=9;
+     t1=f->link;
+     t2=f->link->link;
+     t1->link=t3;
+     t3->link=t2;
+     while(f!=NULL)
+     {
+         printf("%d  ",f->data);
+         f=f->link;
+     }
+}
+>>>>>>> 745d9055431b073d56692add3b0c158869ea679a
